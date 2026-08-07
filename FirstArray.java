2d90727133b213084e79a76add302f98781e9a68
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-07 21:34:18 +0530

    commmited

--- FirstArray.java
+++ FirstArray.java
@@ -4,22 +4,24 @@ public class FirstArray{
   public static void main(String[] args){
     //declare and initialize with values
     int[] arr1 ={ 1,2,3,4,5};
-    //or
-    // int arr1[] = {1,2,3,4,5};
-    int[] arr;
-    //arr={1,2,3,4}
-
+    System.out.println("arr1: " + Arrays.toString(arr1));
+    
     // using new keyword
     int[] arr2 = new int[5];
+    System.out.println("arr2: " + Arrays.toString(arr2));
 
     // declare and initialize using new with values
     int[] arr3 = new int[]{1,2,3,4,5};
+    System.out.println("arr3: " + Arrays.toString(arr3));
 
     int[] arr4;
     arr4 = new int[]{1,2,3,4,5};
+    System.out.println("arr4: " + Arrays.toString(arr4));
 
-    Scanner input = new Scanner(System.in);
-    System.out.print("enter the size of array: ");
-    int n = input.nextInt();
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("enter the size of array: ");
+      int n = input.nextInt();
+      System.out.println("Array size: " + n);
+    }
   }
 }
