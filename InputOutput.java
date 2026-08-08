2128c81dcee76546fd0ee4a73c3d2be2fc0f68da
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-08 09:07:54 +0530

    Standard input output in java

---
+++ InputOutput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class InputOutput{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a=input.nextInt();
+        int b=input.nextInt();
+        int c=input.nextInt();
+        System.out.printf("%d\n%d\n%d",a,b,c);
+        input.close();
+}
+}
