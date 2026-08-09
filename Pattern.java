12cedafa943314e44b1b193b5eab0b696fcaedf9
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-09 15:59:26 +0530

    Star pattern

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pattern{
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter number of lines: ");
+    int n = input.nextInt();
+
+    for (int i=1;i<=n;i++){
+      for (int j=0;j<i;j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+    input.close();
+  }
+}
