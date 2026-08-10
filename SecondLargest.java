c7465aec158016f93bae3bfac897442c6e75d103
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-10 22:59:22 +0530

    second largest element of array

--- SecondLargest.java
+++ SecondLargest.java
@@ -3,14 +3,13 @@ import java.util.*;
 public class SecondLargest{
   public static void display(int arr[]){
 
-    System.out.println("\nArray Elements: ");
     for(int ele : arr){
       System.out.printf("%d ",ele);
     }
   }
   public static void bubbleSort(int arr[]){
 
-    for (int i = 0; i < arr.length - 1 ; i++) {
+    for (int i = 0; i < 2 ; i++) {
         for (int j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
@@ -33,10 +32,8 @@ public class SecondLargest{
       arr[i] = sc.nextInt();
     }
     
-    System.out.println("Og array: ");
-    display(arr);
     bubbleSort(arr);
-    System.out.println("\n\nSecond largest: " + arr[arr.length-2]);
+    System.out.println("Second largest: " + arr[arr.length-2]);
 
     sc.close();
     }
