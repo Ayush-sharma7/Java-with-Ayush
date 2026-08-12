f4ad65448c656150e684ab330812ad24affd117a
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-12 14:56:51 +0530

    Center of three numbers program added

---
+++ CenterNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class CenterNumber {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter three numbers: ");
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      System.out.println((a+b+c)-Math.max(Math.max(a,b),c)-Math.min((Math.min(a,b)),c));
+      sc.close();
+    }
+}
