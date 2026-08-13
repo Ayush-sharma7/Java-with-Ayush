693a87c8f9090aef81f2130c8ce594f9ca2f426b
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-13 12:16:46 +0530

    Number pattern generator for given lines

---
+++ Patterns.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Patterns{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no. of lines");
+        int n = sc.nextInt();
+
+        int x=1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d ",x);
+                x++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
