44fc5b259f5e023eb2437d108c8f5e60b06b800c
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-13 12:44:12 +0530

    Star pattern generator with user-defined lines

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-public class Pattern{
-  public static void main(String[] args){
+public class Pattern {
+  public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
 
     System.out.print("Enter number of lines: ");
     int n = input.nextInt();
 
-    for (int i=1;i<=n;i++){
-      for (int j=0;j<i;j++){
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
         System.out.print("* ");
       }
       System.out.println();
