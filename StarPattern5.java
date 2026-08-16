e2ebf184330310ec2a2f4338cfbc999eccdf981c
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-17 00:00:26 +0530

    Refactor formatting in multiple StarPattern classes for improved readability

--- StarPattern5.java
+++ StarPattern5.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-public class StarPattern5{
-    public static void main(String[] args){
+public class StarPattern5 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of lines: ");
         int n = sc.nextInt();
 
-        for (int i = n; i>=1; i--){
-            for (int j = 1;j<=i;j++){
-                System.out.printf("%s ","*");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%s ", "*");
             }
             System.out.println();
         }
