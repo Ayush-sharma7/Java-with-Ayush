0dc47ee37892fde7121dbb8b5ac533123eed08eb
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-08-16 23:55:10 +0530

    Refactor formatting in StarPattern8 for improved readability

--- StarPattern8.java
+++ StarPattern8.java
@@ -1,21 +1,20 @@
 import java.util.*;
 
-public class StarPattern8{
-    public static void main(String[] args){
+public class StarPattern8 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter number of lines: ");
         int n = sc.nextInt();
 
-        for (int i = 1; i<=n ; i++){
-            for (int k = i; k<n ;k++){
+        for (int i = 1; i <= n; i++) {
+            for (int k = i; k < n; k++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j<=i; j++){
-                if(j==1 || j==i || i==n){
-                    System.out.printf("%s ","*");
-                }
-                else{
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.printf("%s ", "*");
+                } else {
                     System.out.print("  ");
                 }
             }
