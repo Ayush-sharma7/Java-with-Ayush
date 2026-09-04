6e08d0f2dbdca7dfab85198b074b3d1a14665bea
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-09-04 19:40:51 +0530

    Add Question7 class to calculate total, max, min, and count above average durations

---
+++ Question7.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Question7 {
+
+    public static int calcTotal(int arr[]) {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    public static int findMax(int arr[]) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int arr[]) {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int countAboveAverage(int arr[]) {
+        double avg = calcTotal(arr) / arr.length;
+        int c = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > avg) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Durations: ");
+        int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.printf("Total Minutes: %d\nAverage Duration: %.2f\nMax Duration: %d mins | Min Duration: %d mins\nAgents above average: %d" ,calcTotal(arr),calcTotal(arr)/(arr.length*1.0),findMax(arr),findMin(arr),countAboveAverage(arr));
+
+        sc.close();
+    }
+}
--- Question8.java
+++
@@ -1,5 +0,0 @@
-import java.util.*;
-
-public class Question8 {
-    public static void 
-}
