87cd7e8cc93b090d504282bed3bd7744cb3f705f
Ayush-sharma7 <ayushhsharmaa7@gmail.com>
2026-09-14 12:51:24 +0530

    Refactor InheritanceDemo1 class to improve constructor clarity and maintainability

--- InheritanceDemo1.java
+++ InheritanceDemo1.java
@@ -2,6 +2,14 @@ class A {
     int money;
     private int pocketMoney;
 
+    A(){
+        System.out.println("A Class Const.");
+    }
+
+    A(int a, int b){
+        System.out.println("A Class Parameterized Const.");
+    }
+
     void fill(int money, int pocketMoney) {
         this.money = money;
         this.pocketMoney = pocketMoney;
@@ -15,8 +23,13 @@ class A {
 class B extends A {
     int total;
 
+    B(){
+        super(10,20);    //way to call parameterized super class constructor. it acts as a place holder for super class.
+        System.out.println("B class Const.");
+    }
+
     void sum() {
-        total = money + getPocketMoney(); // wont run because pocketMoney is private element.
+        total = money + getPocketMoney();   // wont run because pocketMoney is private element.
     }
 }
 
